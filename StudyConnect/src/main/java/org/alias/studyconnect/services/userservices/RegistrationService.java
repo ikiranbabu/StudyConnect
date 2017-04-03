a5bc58cd3442c3f783347551aad9206ae2ca76b6
Ali Asgar <ali.asgar26@gmail.com>
2017-04-03 04:33:01 -0400

    adding persistence and injecting entity manager

--- StudyConnect/src/main/java/org/alias/studyconnect/services/userservices/RegistrationService.java
+++ StudyConnect/src/main/java/org/alias/studyconnect/services/userservices/RegistrationService.java
@@ -1,20 +1,23 @@
 package org.alias.studyconnect.services.userservices;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
 import org.alias.studyconnect.model.UserDetails;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 
 public class RegistrationService {
-
+	private EntityManager entityManager;
 	
 	public UserDetails registerUser(UserDetails user){
-		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-		session.save(user);
-		session.getTransaction().commit();
-		session.close();
+		entityManager = Persistence.createEntityManagerFactory("studyconnect")
+				.createEntityManager();
+ 
+		entityManager.getTransaction().begin();
+ 
+		entityManager.persist(user);
+ 
+		entityManager.getTransaction().commit();
+		entityManager.close();
 		return user;
 	}
 	
